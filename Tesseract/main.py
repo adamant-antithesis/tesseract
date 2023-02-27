0c787859db43a01110fae7a308f50f68f31ace02
Adamant <zt.gaming.sh@gmail.com>
2023-02-27 13:40:45 +0200

    adding custom_config

--- Tesseract/main.py
+++ Tesseract/main.py
@@ -4,5 +4,7 @@ from PIL import Image
 img = Image.open('number.jpg')                                                        #name of your file
 pytesseract.pytesseract.tesseract_cmd = r"D:\Program Files\Tesseract\tesseract.exe"   #path to Tesseract
 
-text = pytesseract.image_to_string(img)
+# custom_config = r'--oem 3'                                                            #custom config, read - https://help.ubuntu.ru/wiki/tesseract
+
+text = pytesseract.image_to_string(img)                                               #add config=custom_config as second argument
 print(text)
